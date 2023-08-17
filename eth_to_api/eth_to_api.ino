3e81ee78155279a2eecbf9a895813333397b7bf9
msc_intra <109829955+mscrobotics@users.noreply.github.com>
2023-08-17 15:37:52 -0800

    Update eth_to_api.ino

--- eth_to_api/eth_to_api.ino
+++ eth_to_api/eth_to_api.ino
@@ -1,9 +1,33 @@
+/*
+Connect To Ethernet - setup();
+Collect Data From FGM - shiftPin(); collectData();
+Convert Data To Magnetic Field - freqToMag();
+Get A Timestamp - loop();
+Send Data To API - sendDataToAPI();
+*/
+
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetClient.h>
+#include <FreqCount.h> // https://github.com/PaulStoffregen/FreqCount/releases/tag/1.3
+
+// Global Variables
+
+const int baudRate = 9600; // Serial Baud Rate
+const int gateInterval = 1000; // The time in milliseconds for each measurement. - https://www.pjrc.com/teensy/td_libs_FreqCount.html#:~:text=FreqCount.begin,a%20scale%20factor
+unsigned long lastDataSentTime = 0;
 
 const char* hostName = "msmcs-robotics.github.io"; // Change this to your server's hostname or domain
 const char* apiPath = "/CosmicGlimpse/api/temperature"; // Change this to your API endpoint path
+
+const int fgmPowerPinX = 4;
+const int fgmPowerPinY = 5;
+const int fgmPowerPinZ = 6;
+
+const int fgmDataPinX = 3;
+const int fgmDataPinY = 9;
+const int fgmDataPinZ = 10;
 
 const int sensorPin = A0; // Assuming your temperature sensor is connected to analog pin A0
 int temperature = 0;
@@ -18,17 +42,82 @@ byte mac[] = {
 
 EthernetClient client;
 
+/* 1. Send power to flux-gate-magnatometer power pins from arduino pins one at a time
+2. Read from flux-gate-magnatometer data pins to arduino one at a time
+3. Interpret data as a frequency
+4. Store resulting frequency from a specified time interval in a single variable
+*/
+
+
+// Produce a function to map X, Y, and Z frequencies into a numerical representation of a magnetic field
+int freqToMag() {
+    int bigB = 0; // Set magnetic field to 0 to start 
+
+    // Pass frequencies from X, Y, and Z axi
+    // Use the equation: B = a + bf + cf^2
+
+    return bigB;
+};
+
+void shiftPin(int powerPin, int dataPin, bool collectState) {
+    // Change which pin is being used to collect frequencies - X, Y, or Z
+
+    if ( collectState == false ) {
+        digitalWrite(powerPin, LOW); // Turn off power to sensor pin
+    } else if ( collectState == true ) {
+        digitalWrite(powerPin, HIGH); // Turn on power to sensor pin
+    } else {
+        Serial.println("Incorrect 'collectState' in shiftPin function");
+    }
+};
+
+void collectData() {
+    // Collect an average of frequencies and process the data into a single variable
+    if (FreqCount.available()) {
+        unsigned long count = FreqCount.read();
+        Serial.println(count);
+    }
+};
+
+void sendDataToAPI(int data) {
+  if (client.connect(server, serverPort)) {
+    // Create the POST request body
+    String postData = "temperature=" + String(data);
+
+    // Get the current timestamp in seconds since the Arduino started
+    unsigned long currentTime = millis() / 1000;
+    postData += "&timestamp=" + String(currentTime);
+
+    // Make the HTTP POST request
+    client.println("POST " + String(apiPath) + " HTTP/1.1");
+    client.print("Host: ");
+    client.println(String(hostName));
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+    client.println();
+
+    delay(1000); // Wait for a second before sending the next request
+
+    client.stop(); // Close the connection
+  }
+  Serial.print("Sent ");
+  Serial.print(data);
+  Serial.print(" to ");
+  Serial.println(String(hostName) + String(apiPath));
+}
+
 void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH Shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
 
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(baudRate);
+  FreqCount.begin(gateInterval);
+
+  pinMode(fgmDataPin1, INPUT);
+  pinMode(fgmDataPin2, INPUT);
+  pinMode(fgmDataPin3, INPUT);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -50,32 +139,6 @@ void setup() {
   // print your local IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
-}
-
-void sendDataToAPI(int data) {
-  if (client.connect(server, serverPort)) {
-    // Create the POST request body
-    String postData = "temperature=" + String(data);
-
-    // Make the HTTP POST request
-    client.println("POST " + String(apiPath) + " HTTP/1.1");
-    client.print("Host: ");
-    client.println(String(hostName));
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.print("Content-Length: ");
-    client.println(postData.length());
-    client.println();
-    client.println(postData);
-    client.println();
-
-    delay(1000); // Wait for a second before sending the next request
-
-    client.stop(); // Close the connection
-  }
-  Serial.print("Sent ");
-  Serial.print(data);
-  Serial.print(" to ");
-  Serial.println(String(hostName) + String(apiPath));
 }
 
 void loop() {
@@ -114,6 +177,13 @@ void loop() {
   // Read temperature from the sensor (you might need to use the correct method for your sensor)
   temperature = 10;
 
-  // Send data to API
-  sendDataToAPI(temperature);
+  // Timestamp
+  // Check if enough time has passed since the last data was sent
+  if (millis() - lastDataSentTime >= 1000) { // Adjust the time interval as needed
+    // Send data to API
+    sendDataToAPI(temperature);
+
+    // Update the last data sent time
+    lastDataSentTime = millis();
+  }
 }
